dfd7b65c242ad2a80585dff4ee79ada7063fad7f
Sanjay17u <sanjay.solanki.17.om@gmail.com>
2026-02-05 16:49:12 +0530

    W3D4P2 - completed

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { useIsFocused } from '@react-navigation/native'
+import { useEffect, useState } from 'react'
+
 
 
 
 function Home() {
 
     const isFocused = useIsFocused()
+    const [message, setMessage] = useState('')
+
+    useEffect(() => {
+        if (isFocused) {
+            setMessage('Home Screen Active ✅')
+        } else {
+            setMessage('')
+        }
+    }, [isFocused])
+    
 
     return (
         <>
             <View>
-                <Text>Status: {isFocused ? 'Focused ✅' : 'Blurred ❌' }</Text>
+                <Text>{ message }</Text>
             </View>
         </>
     )
