b704c17b7749f1c7acc6aae154b8ab81f6656c50
Sanjay17u <sanjay.solanki.17.om@gmail.com>
2026-02-12 16:43:16 +0530

    W3 Finaly Completed

--- src/component/DetailScreen.js
+++ src/component/DetailScreen.js
@@ -4,26 +4,40 @@ import { View, Text, Button } from 'react-native'
 function DetailScreen({ navigation }) {
   
   const [message, setMessage] = useState('Details Screen')
+  const [count, setCount] = useState(0)
   
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerRight: () => (
-        <Button
-          title="Change"
-          onPress={() => setMessage('Header Button Pressed 🚀')}
+      title: `Details (${count})`,
+
+      headerStyle: {
+      backgroundColor: 'black',
+    },
+      headerTintColor: 'white',
+    
+      headerBackVisible: false,
+
+    headerLeft: () => (
+      <Button
+        title="Back"
+        onPress={() => navigation.goBack()}
+        color="blue"
       />
-      )
+    ),
+
+    headerRight: () => (
+      <Button
+        title="Toggle"
+        onPress={() => setCount(count + 1)}
+        color="blue"
+      />
+    ),
     })
-  }, [navigation])
+  }, [navigation, count])
 
   return (
     <View>
       <Text>{ message }</Text>
-
-      <Button
-        title="Go Back"
-        onPress={() => navigation.goBack()}
-      />
     </View>
   )
 }
